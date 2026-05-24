2b973582114790b9a662023ed0a5caa3a22b69ef
Makuochukwu Okoene <okoenemakuo04@outlook.com>
2026-05-24 09:08:41 +0200

    added network repo to track connectivity state

--- app/src/main/java/fr/epita/snapquest/data/repo/NetworkStateRepository.java
+++ app/src/main/java/fr/epita/snapquest/data/repo/NetworkStateRepository.java
@@ -1,4 +1,26 @@
 package fr.epita.snapquest.data.repo;
 
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 public class NetworkStateRepository {
+
+    private static final NetworkStateRepository INSTANCE = new NetworkStateRepository();
+
+    private final MutableLiveData<Boolean> online = new MutableLiveData<>(true);
+
+    private NetworkStateRepository() { }
+
+    public static NetworkStateRepository get() {
+        return INSTANCE;
+    }
+
+    public LiveData<Boolean> networkAvailable() {
+        return online;
+    }
+
+    /** Called by ConnectivityReceiver on the main thread. */
+    public void setOnline(boolean isOnline) {
+        online.postValue(isOnline);
+    }
+
 }
